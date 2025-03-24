8bbaa381c5eb7231de72be69b9229809528ae3ee
Lawrence <xiaoxuan.li.w@gmail.com>
2025-03-25 00:08:10 +1100

    add intersentence

--- code/run.sh
+++ code/run.sh
@@ -4,20 +4,20 @@ python eval_generative_models.py \
   --pretrained-class meta-llama/Llama-3.2-3B-Instruct \
   --intrasentence-model LLaMALM \
   --intrasentence-load-path ../../PhD_causalDPO/.cache/root/hh_dpo_llama3b_random_2025-03-23_00-13-18_121917/LATEST/policy.pt \
+  --intersentence-model LLaMALM \
   --tokenizer AutoTokenizer \
   --batch-size 1 \
   --input-file ../data/dev.json \
   --output-dir ./predictions \
   --unconditional_start_token "<s>" \
-  --skip-intersentence
 
   python eval_generative_models.py \
   --pretrained-class meta-llama/Llama-3.2-3B-Instruct \
   --intrasentence-model LLaMALM_debiased \
   --intrasentence-load-path ../../PhD_causalDPO/.cache/root/hh_dpo_llama3b_random_stereo_debias_2025-03-23_06-00-07_066205/LATEST/policy.pt \
+  --intersentence-model LLaMALM_debiased \
   --tokenizer AutoTokenizer \
   --batch-size 1 \
   --input-file ../data/dev.json \
   --output-dir ./predictions \
   --unconditional_start_token "<s>" \
-  --skip-intersentence
