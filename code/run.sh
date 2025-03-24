8451a893f57c8464d3581c6481fbfbf015486c10
Lawrence <xiaoxuan.li.w@gmail.com>
2025-03-24 23:04:36 +1100

    edit run.sh

--- code/run.sh
+++ code/run.sh
@@ -2,7 +2,7 @@ python eval_generative_models.py \
   --pretrained-class meta-llama/Llama-3.2-3B-Instruct \
   --intrasentence-model LLaMALM \
   --intrasentence-load-path ../../PhD_causalDPO/.cache/root/hh_dpo_llama3b_random_2025-03-23_00-13-18_121917/LATEST/policy.pt \
-  --tokenizer LlamaTokenizer \
+  --tokenizer AutoTokenizer \
   --batch-size 1 \
   --input-file ../data/dev.json \
   --output-dir ./predictions \
