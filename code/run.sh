8cc47b6d080b02861461711aef41b65d5d823cd1
Lawrence Li <ghost651373019@gmail.com>
2025-03-31 00:50:29 +1100

    edit script

--- code/run.sh
+++ code/run.sh
@@ -2,9 +2,9 @@
 
 python eval_generative_models.py \
   --pretrained-class meta-llama/Llama-3.2-1B-Instruct \
-  --intrasentence-model LLaMALM \
+  --intrasentence-model LLaMALM_debiased \
   --intrasentence-load-path /workspace/LATEST/policy.pt \
-  --intersentence-model LLaMALM \
+  --intersentence-model LLaMALM_debiased \
   --tokenizer AutoTokenizer \
   --batch-size 1 \
   --input-file ../data/dev.json \
