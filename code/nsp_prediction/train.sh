0443ca8ef2c8f58ef33e8abe4bdba78502b1b61e
Lawrence Li <ghost651373019@gmail.com>
2025-04-05 14:01:38 +1100

    training script for NSP head

---
+++ code/nsp_prediction/train.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+nohup python main.py \
+  --tokenizer AutoTokenizer \
+  --model LlamaModel \
+  --pretrained-class meta-llama/Llama-3.2-1B-Instruct \
+  --dataset ./process_wikipedia/output \
+  --batch-size 4 \
+  --epochs 3 \
+  --max-seq-length 256 \
+  --core-lr 5e-6 \
+  --head-lr 1e-3 \
+  --accumulation-steps 8 \
+  --fp16 \
+  --opt O1 > llama_nsp_training.log 2>&1 &
